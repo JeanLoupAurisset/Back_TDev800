b1264d614a0ed95a671f7726a9567086927aa87a
BigShoes <jeanloup.aurisset@gmail.com>
2021-06-24 11:49:18 +0200

    Models ok

--- api/api/models.py
+++ api/api/models.py
@@ -9,10 +9,10 @@ class utilisateur(models.Model):
     date_inscription = models.DateField()
 
 class device(models.Model):
-   id_device = models.IntegerField(primary_key=True)
-   nom = models.CharField(max_length=50)
-   marque = models.CharField(max_length=50)
-   modele = models.CharField(max_length=50)
+    id_device = models.IntegerField(primary_key=True)
+    nom = models.CharField(max_length=50)
+    marque = models.CharField(max_length=50)
+    modele = models.CharField(max_length=50)
 
 class album(models.Model):
     id_album = models.IntegerField(primary_key=True)
@@ -25,12 +25,12 @@ class photo(models.Model):
     id_utilisateur = models.ForeignKey(utilisateur, on_delete=models.CASCADE)
 
 class metadata(models.Model):
-   id_metadata = models.IntegerField(primary_key=True)
-   type = models.CharField(max_length=50)
-   valeur = models.CharField(max_length=50)
-   mode_acquisition = models.CharField(max_length=50)
-   date_ajout = models.DateField()
-   id_photo = models.ForeignKey(photo, on_delete=models.CASCADE)
+    id_metadata = models.IntegerField(primary_key=True)
+    type = models.CharField(max_length=50)
+    valeur = models.CharField(max_length=50)
+    mode_acquisition = models.CharField(max_length=50)
+    date_ajout = models.DateField()
+    id_photo = models.ForeignKey(photo, on_delete=models.CASCADE)
 
 class regrouper(models.Model):
     id_photo = models.ForeignKey(photo, on_delete=models.CASCADE, primary_key=True)
