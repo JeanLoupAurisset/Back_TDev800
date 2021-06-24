4dbe67402a3e96fa308dea1c4c35acb503706e02
BigShoes <jeanloup.aurisset@gmail.com>
2021-06-24 11:09:23 +0200

    ajouts fichiers djangos

---
+++ api/api/models.py
@@ -0,0 +1,23 @@
+(myprojectenv) [root@pga bin]# cat cars/models.py 
+
+from django.db import models
+
+
+
+class Driver(models.Model):
+
+    name = models.TextField()
+
+    license = models.TextField()
+
+class Car(models.Model):
+
+    make = models.TextField()
+
+    model = models.TextField()
+
+    year = models.IntegerField()
+
+    vin = models.TextField()
+
+    owner = models.ForeignKey("Driver", on_delete=models.SET_NULL, null=True)
--- api/api/settings.py
+++ api/api/settings.py
@@ -74,10 +74,23 @@ WSGI_APPLICATION = 'api.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'tdev_DB',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'postgres',
+
+        'HOST': 'postgres',
+
+        'PORT': '5432',
+
     }
+
 }
 
 
