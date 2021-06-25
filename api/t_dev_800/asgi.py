2bb800c8dce58350a1470958ad8a27efd6ef49e4
BigShoes <jeanloup.aurisset@gmail.com>
2021-06-25 08:32:49 +0200

    Revert "fix appels"
    This reverts commit 5639ba3ac52a443ca4bb4b7a3d7602ba2a271a46.

--- api/t_dev_800/asgi.py
+++ api/t_dev_800/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 't_dev_800.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
 
 application = get_asgi_application()
--- api/t_dev_800/wsgi.py
+++ api/t_dev_800/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 't_dev_800.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
 
 application = get_wsgi_application()
