954f26ef5a4c8e59fdf07a38eeaf304cef1315d5
BigShoes <jeanloup.aurisset@gmail.com>
2021-07-07 15:00:26 +0200

    Dockerfile maison

--- api/devops/deploy.sh
+++ api/devops/deploy.sh
@@ -3,5 +3,5 @@ ssh -o StrictHostKeyChecking=no jl@176.128.78.27 << 'ENDSSH'
  cd /your_project_name
  docker login -u $REGISTRY_USER -p $CI_BUILD_TOKEN $CI_REGISTRY
  docker pull registry.gitlab.com/BigShoes/django/djangopg:latest
- docker run --name api -p 8000:8000 -d djangopg
+ docker run --name tdev800 -p 8000:8000 -d djangopg
 ENDSSH
