06527c3920f99606cde5b2eb0d104a8557ed7294
BigShoes <jeanloup.aurisset@gmail.com>
2021-07-07 16:35:44 +0200

    ajout sshpass

--- api/devops/deploy.sh
+++ api/devops/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-ssh -o StrictHostKeyChecking=no jl@176.128.78.27 << 'ENDSSH'
+sshpass -f <(printf '%s\n' $VM_PASS) ssh -o StrictHostKeyChecking=no jl@176.128.78.27 << 'ENDSSH'
  cd /django
  docker login -u $REGISTRY_USER -p $CI_BUILD_TOKEN $CI_REGISTRY
  docker pull registry.gitlab.com/bigshoes/django/djangopg:latest
