724ff040750c6098728fcc2ed049d2cc866726d7
BigShoes <jeanloup.aurisset@gmail.com>
2021-07-07 15:16:38 +0200

    lower big shoes

--- api/devops/deploy.sh
+++ api/devops/deploy.sh
@@ -2,6 +2,6 @@
 ssh -o StrictHostKeyChecking=no jl@176.128.78.27 << 'ENDSSH'
  cd /your_project_name
  docker login -u $REGISTRY_USER -p $CI_BUILD_TOKEN $CI_REGISTRY
- docker pull registry.gitlab.com/BigShoes/django/djangopg:latest
+ docker pull registry.gitlab.com/bigshoes/django/djangopg:latest
  docker run --name tdev800 -p 8000:8000 -d djangopg
 ENDSSH
